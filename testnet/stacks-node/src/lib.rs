66071ab6e9a6bae3e3a164c0fea1f3d8dd8f3ca7
Aaron Blankstein <aaron@hiro.so>
2025-03-31 11:22:22 -0500

    fix: move global alloc declaration to main from lib

--- testnet/stacks-node/src/lib.rs
+++ testnet/stacks-node/src/lib.rs
@@ -41,8 +41,6 @@ use stacks::chainstate::stacks::db::blocks::DummyEventDispatcher;
 use stacks::chainstate::stacks::db::StacksChainState;
 use stacks::config::chain_data::MinerStats;
 pub use stacks::config::{Config, ConfigFile};
-#[cfg(not(any(target_os = "macos", target_os = "windows", target_arch = "arm")))]
-use tikv_jemallocator::Jemalloc;
 
 pub use self::burnchains::{
     BitcoinRegtestController, BurnchainController, BurnchainTip, MocknetController,
@@ -54,10 +52,6 @@ pub use self::run_loop::{helium, neon};
 pub use self::tenure::Tenure;
 use crate::neon_node::{BlockMinerThread, TipCandidate};
 use crate::run_loop::boot_nakamoto;
-
-#[cfg(not(any(target_os = "macos", target_os = "windows", target_arch = "arm")))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
 
 /// Implmentation of `pick_best_tip` CLI option
 fn cli_pick_best_tip(config_path: &str, at_stacks_height: Option<u64>) -> TipCandidate {
--- testnet/stacks-node/src/main.rs
+++ testnet/stacks-node/src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(any(target_os = "macos", target_os = "windows", target_arch = "arm")))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(any(target_os = "macos", target_os = "windows", target_arch = "arm")))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 fn main() {
     stacks_node::entry_main();
 }
